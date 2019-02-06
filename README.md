144e36448d30df1f554d3d92e8a5ab3e7e365d71
MuhamadAgamLesmana <agamlesmana@gmail.com>
2019-02-06 19:21:24 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Apa itu Vue js?
+
+VueJS adalah kerangka kerja JavaScript progresif yang digunakan untuk mengembangkan antarmuka web interaktif. Fokus lebih pada bagian tampilan, yang merupakan ujung depan. Sangat mudah untuk diintegrasikan dengan proyek dan perpustakaan lain. Instalasi VueJS cukup sederhana, dan pemula dapat dengan mudah memahami dan mulai membangun antarmuka pengguna mereka sendiri. Konten tersebut dibagi menjadi beberapa bab yang berisi topik terkait dengan contoh-contoh sederhana dan berguna.
+
+## Tugas
+
+Berikut adalah tugas vue.js membuat 3 soal beserta pilihan jawabannya yang dimana jika memilih jawaban yang benar nilai akan bertambah 1 
+
+Muhamad Agam Lesmana
+XII RPL 3
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h1>{{ nama }}</h1>
+    <input type="text" v-model="nama">
+    <br><br>
+    Nilai Anda : {{ nilai }}
+    <br>    
+    <h2>{{ soal1 }}</h2>
+    <input type="radio" name="jawaban1" :disabled="checked" @click="benar"> Susilo Bambang Yudhoyono
+    <input type="radio" name="jawaban1" :disabled="checked" > Sisilo Bambang Yaris
+    <input type="radio" name="jawaban1" :disabled="checked"> Susilo Bopung Yandi
+    <h2>{{ soal2 }}</h2>
+    <input type="radio" name="jawaban2" :disabled="checked2"> hidung
+    <input type="radio" name="jawaban2" :disabled="checked2"> telinga
+    <input type="radio" name="jawaban2" :disabled="checked2" @click="benar2"> Insang
+    <h2>{{ soal3 }}</h2>
+    <input type="radio" name="jawaban3" :disabled="checked3" @click="benar3"> 24
+    <input type="radio" name="jawaban3" :disabled="checked3" > 25
+    <input type="radio" name="jawaban3" :disabled="checked3"> 23
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      nama: 'Nama Anda',
+      nilai: 0,
+      soal1: '1. Siapa nama lengkap SBY ?',
+      soal2: '2. Ikan bernafas menggunakan ?',
+      soal3: '3. Ada berapa total huruf hijaiyah" adalah......'
+    }
+  },
+  methods:{
+    benar(){
+      this.nilai += 1,
+      this.checked = true
+    },
+     benar2(){
+      this.nilai += 1,
+      this.checked2 = true
+    },
+     benar3(){
+      this.nilai += 1,
+      this.checked3 = true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  font-size: 30px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
